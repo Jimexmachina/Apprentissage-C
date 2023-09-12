c9cfb9a97d0b776e2e70340a665554cf03804245
Jimexmachina <144821072+Jimexmachina@users.noreply.github.com>
2023-09-12 19:01:37 +0200

    Update and rename variables.txt to variables.c

---
+++ variables.c
@@ -0,0 +1,20 @@
+/*https://home.csulb.edu/~wmartinz/sites/default/files/images/jan00-03.JPG*/
+#include <stdio.o>
+
+int main(void)
+{
+  /*
+  %d = int
+  %f = float
+  %c = char (déconseillé)
+  %s = chaîne de caractères (texte)
+  */
+
+  float prix = 10.00;
+  const int tva = 20;
+  int prix2 = 8;
+  float prix3 = 6.00;
+  
+
+  printf("Le prix d'un paquet de cigarette est de %.2f, parce que la tva est de %d sinon le paquet serait à %d. Avant il était à %f /n", prix, tva, prix2, prix3);
+}
