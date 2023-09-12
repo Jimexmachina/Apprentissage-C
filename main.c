5579f46eebe983dde66cef9fb189e6ab9932fc7a
Jimexmachina <144821072+Jimexmachina@users.noreply.github.com>
2023-09-12 17:23:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
-
+/*
+Inclusion de la Bibliothèque "stdio.h pour la fonction C "printf"
+*/
+#include <stdio.h>
+// Avec C chaque function doit retourner quelque chose (return 0 parce que la fonction est un integer(entier))
+int main(void)
+{
+    printf("Bonjour tout le monde \n");
+  /*Ici on retourne 0 comme dit plus haut*/
+    return 0;
+}
