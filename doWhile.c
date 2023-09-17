63aaa3adeffbfc3767c4a4a77586c98e5b459946
Jimexmachina <144821072+Jimexmachina@users.noreply.github.com>
2023-09-17 16:35:15 +0200

    Update doWhile.c

--- doWhile.c
+++ doWhile.c
@@ -1,18 +1,33 @@
-// DO WHILE
+// Demander à l’utilisateur de saisir des notes(entre 0 et 20)
+// Expliquer qu’une valeur hors de cet intervalle arrêtera la saisie.
+
+//  Compter les notes saisies.Une fois la saisie terminée,
+//  Afficher le nombre de notes saisies.
+
 #include <stdio.h>
 
 int main(void)
 {
-    int x = 0;
-    int y = 15;
-// Expression (do).
+    float notes = 0;
+    int nbreNotes = 0;
+    float sommeNotes = 0;
+    float moyenneNotes = 0;
+
+    printf("Saisissez les notes, pour arréter la saisie, rentrez une note hors de l'interval (0 à 20) ");
+
     do
     {
-        x++;
-        printf("%d \n", x);
 
-    //L'instruction est répété tant que la condition n'est pas TRUE (while)
-    } while (x < y);
+        scanf("%f/n", &notes);
+        sommeNotes += notes;
+        nbreNotes += 1;
+
+    } while (notes >= 0 && notes <= 20);
+
+    nbreNotes -= 1;
+    moyenneNotes = sommeNotes / nbreNotes;
+
+    printf("La somme des notes est de %.2f", sommeNotes);
 
     return 0;
 }
